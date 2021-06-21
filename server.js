6813f62ea4d92b02cc3352762e66273f1adcf18f
nhanhieuhcmus <vosihieunhan@gmail.com>
2021-06-21 10:14:26 +0700

    update server.js

--- server.js
+++ server.js
@@ -71,8 +71,11 @@ function sendMessage(senderId, message) {
   });
 }
 
-app.set('port', process.env.OPENSHIFT_NODEJS_PORT || process.env.PORT || 3002);
-app.set('ip', process.env.OPENSHIFT_NODEJS_IP || process.env.IP || "127.0.0.1");
+// app.set('port', process.env.OPENSHIFT_NODEJS_PORT || process.env.PORT || 3002);
+// app.set('ip', process.env.OPENSHIFT_NODEJS_IP || process.env.IP || "127.0.0.1");
+
+app.set('port', process.env.PORT || 3000);
+app.set('ip',  process.env.IP || "127.0.0.1");
 
 server.listen(app.get('port'), app.get('ip'), function() {
   console.log("Chat bot server listening at %s:%d ", app.get('ip'), app.get('port'));
